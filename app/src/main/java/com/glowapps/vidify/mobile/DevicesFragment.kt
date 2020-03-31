ca54bcd0bba0309f8d9aa085113f9ecc6b6dcd15
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-03-31 19:07:49 +0200

    Better margins, empty view

--- app/src/main/java/com/glowapps/vidify/mobile/DevicesFragment.kt
+++ app/src/main/java/com/glowapps/vidify/mobile/DevicesFragment.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.glowapps.vidify.R
 import com.glowapps.vidify.mobile.presenter.DeviceCardAdapter
 
+
 class DevicesFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
+    private lateinit var emptyView: TextView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
@@ -22,6 +25,7 @@ class DevicesFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.mobile_fragment_devices, container, false)
+        emptyView = root.findViewById(R.id.empty_view) as TextView
 
         viewManager = LinearLayoutManager(activity!!)
         viewAdapter = DeviceCardAdapter()
@@ -37,6 +41,9 @@ class DevicesFragment : Fragment() {
             // specify an viewAdapter (see also next example)
             adapter = viewAdapter
         }
+
+        recyclerView.visibility = View.GONE
+        emptyView.visibility = View.VISIBLE
 
         return root
     }
