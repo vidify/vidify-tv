f71b0974ea85de466fcb204e6436039674c62a4a
Mario Ortiz Manero <marioortizmanero@gmail.com>
2020-04-08 09:42:17 +0200

    Closes #17

--- app/src/main/java/com/glowapps/vidify/mobile/HelpFragment.kt
+++ app/src/main/java/com/glowapps/vidify/mobile/HelpFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.glowapps.vidify.R
+import com.glowapps.vidify.util.openURL
 
 class HelpFragment : Fragment() {
     override fun onCreateView(
@@ -13,6 +15,11 @@ class HelpFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.mobile_fragment_help, container, false)
+        val root = inflater.inflate(R.layout.mobile_fragment_help, container, false)
+
+        val image: ImageView = root.findViewById(R.id.website_qrcode)
+        image.setOnClickListener { openURL(activity!!, getString(R.string.website_link)) }
+
+        return root
     }
 }
--- app/src/main/java/com/glowapps/vidify/mobile/MainMobileActivity.kt
+++ app/src/main/java/com/glowapps/vidify/mobile/MainMobileActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -27,18 +28,9 @@ class MainMobileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.mobile_activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
 
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each menu should be considered as top level
-        // destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_devices, R.id.nav_help, R.id.nav_subscribe), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        setupActionBar()
+        setupNavbar()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -52,14 +44,36 @@ class MainMobileActivity : AppCompatActivity() {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
+    // Clickable buttons in the top action bar
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        Log.i(TAG, "$item clicked")
         when (item.itemId) {
             R.id.share_button -> share(this)
-            R.id.github_qrcode -> openURL(this, getString(R.string.github_link))
             R.id.twitter_button -> openURL(this, getString(R.string.twitter_link))
             else -> Log.e(TAG, "Unknown options item selected: ${item.itemId}")
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setupActionBar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    private fun setupNavbar() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level
+        // destinations.
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_devices, R.id.nav_help, R.id.nav_subscribe), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    fun openWebsite() {
+
     }
 }
